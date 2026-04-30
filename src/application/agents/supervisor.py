a93e17e1d34c9cb7c1356ac8b725883e106a836d
Ian Gonzalez <yan26iangonzalezyan26@hotmail.com>
2026-04-30 17:28:46 -0300

    feat: change model for supervisor and update the readme

--- src/application/agents/supervisor.py
+++ src/application/agents/supervisor.py
@@ -1,6 +1,6 @@
 from langchain.chat_models import init_chat_model
 from langchain_core.language_models.chat_models import BaseChatModel
-from src.infrastructure.config import LLM_PROVIDER, QUERY_MODEL
+from src.infrastructure.config import LLM_PROVIDER, GENERATION_MODEL
 from src.domain.prompts.supervisor_prompt_en import SUPERVISOR_PROMPT_EN
 
 AGENTS = ["analyst", "teacher"]
@@ -9,4 +9,4 @@ SUPERVISOR_ROUTING_PROMPT = SUPERVISOR_PROMPT_EN
 
 
 def get_supervisor_llm() -> BaseChatModel:
-    return init_chat_model(model_provider=LLM_PROVIDER, model=QUERY_MODEL)
+    return init_chat_model(model_provider=LLM_PROVIDER, model=GENERATION_MODEL)
